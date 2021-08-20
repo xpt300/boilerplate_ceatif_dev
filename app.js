a79a51ea53857c6d500c9ee3028ee27b9d6af54f
Maxime JOUBERT <m.joubert.92@gmail.com>
2021-08-20 12:16:38 +0200

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.get('/', (req, res) => {
+  res.render('index', {
+    meta: {
+      data: {
+        title: "Maxime Joubert",
+        description: "Test mata data",
+      }
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
